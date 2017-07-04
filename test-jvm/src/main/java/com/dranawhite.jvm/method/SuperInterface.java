1850b9ec3ac6322d5a521dd85353bf88058848c4
liangyuquan <yuquan.liang@mi-me.com>
2017-07-04 18:23:47 +0800

    目录结构重新划分

---
+++ test-jvm/src/main/java/com/dranawhite.jvm/method/SuperInterface.java
@@ -0,0 +1,8 @@
+package com.dranawhite.jvm.method;
+
+/**
+ * @author dranawhite.web.jvm 2017/7/4
+ * @version 1.0
+ */
+interface SuperInterface {
+}
--- test-jvm/src/main/java/com/dranawhite/method/SuperInterface.java
+++
@@ -1,8 +0,0 @@
-package com.dranawhite.method;
-
-/**
- * @author dranawhite 2017/7/4
- * @version 1.0
- */
-interface SuperInterface {
-}
