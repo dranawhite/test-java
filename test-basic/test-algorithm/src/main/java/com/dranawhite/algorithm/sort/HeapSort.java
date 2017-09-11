e166ce88b2dacf4bfccecc05d0fc22eee55979b0
liangyuquan <yuquan.liang@mi-me.com>
2017-09-11 15:27:25 +0800

    堆排序算法

--- test-basic/test-algorithm/src/main/java/com/dranawhite/algorithm/sort/HeapSort.java
+++ test-basic/test-algorithm/src/main/java/com/dranawhite/algorithm/sort/HeapSort.java
@@ -4,7 +4,10 @@ package com.dranawhite.algorithm.sort;
  * 堆排序
  * <pre>
  * 算法思想：
- *      二叉树的数组实现，对于某一节点i，它的父节点为(i-1)/2，它的左子节点为2i+1,右子节点为2i+2
+ *      二叉树的数组实现，对于某一节点i，它的父节点为(i-1)/2，它的左子节点为2i+1,右子节点为2i+2;
+ *      将待排序的序列构造成一个大顶堆。此时，整个序列的最大值就是堆顶的根节点。将它移走（其实就是将其与堆数组
+ * 的末尾元素交换，此时末尾元素就是最大值），然后将剩余的 n-1 个序列重新构造成一个堆，这样就会得到 n 个元素中
+ * 次大的值。如此反复执行，便能得到一个有序序列了
  *
  * 时间复杂度:
  *      O(N*logN)
@@ -12,33 +15,40 @@ package com.dranawhite.algorithm.sort;
  *
  * @author liangyq 2017/8/30
  */
-public class HeapSort extends ComplexSort {
+public class HeapSort extends SelectSort {
 
     @Override
     public void sort(Integer[] arrs) {
-        for (int i = 0, len = arrs.length; i < len - 1; i++) {
-            buildMaxHeap(arrs, len - 1 - i);
-            SortUtils.compareAndSwap(arrs, 0, len - 1 - i);
+        // 构建一个大顶堆
+        for (int i = arrs.length / 2 - 1; i >= 0; i--) {
+            buildMaxHeap(arrs, i, arrs.length - 1);
+        }
+        // 将堆顶最大值放入到数组中，循环构建最大堆
+        for (int i = arrs.length - 1; i >= 0; i--) {
+            SortUtils.compareAndSwap(arrs, 0, i);
+            buildMaxHeap(arrs, 0, i - 1);
         }
     }
 
-    private void buildMaxHeap(Integer[] data, int lastIndex) {
-        for (int i = (lastIndex - 1) / 2; i >= 0; i--) {
-            int k = i;
-            while (k * 2 + 1 <= lastIndex) {
-                int biggerIndex = 2 * k + 1;
-                if (biggerIndex < lastIndex) {
-                    if (data[biggerIndex] < data[biggerIndex + 1]) {
-                        biggerIndex++;
-                    }
-                }
-                if (data[k] < data[biggerIndex]) {
-                    SortUtils.compareAndSwap(data, k, biggerIndex);
-                    k = biggerIndex;
-                } else {
-                    break;
-                }
+    /**
+     * 构建最大堆
+     *
+     * @param arrs
+     * @param idx
+     * @param len
+     */
+    private void buildMaxHeap(Integer[] arrs, int idx, int len) {
+        int temp = arrs[idx];
+        for (int i = 2 * idx; i < len; i = 2 * i) {
+            if (arrs[i] < arrs[i + 1]) {
+                i = i + 1;
             }
+            if (temp >= arrs[i]) {
+                break;
+            }
+            arrs[idx] = arrs[i];
+            idx = i;
         }
+        arrs[idx] = temp;
     }
 }
