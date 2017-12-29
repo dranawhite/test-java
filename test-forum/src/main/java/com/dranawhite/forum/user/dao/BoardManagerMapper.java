adae08181044c36064c78b36564dfb236aea9fa2
dranawhite <1458755325@qq.com>
2017-12-29 17:56:06 +0800

    搭建基础环境

--- test-forum/src/main/java/com/dranawhite/forum/user/dao/BoardManagerMapper.java
+++ test-forum/src/main/java/com/dranawhite/forum/user/dao/BoardManagerMapper.java
@@ -1,11 +1,7 @@
 package com.dranawhite.forum.user.dao;
 
 import com.dranawhite.forum.base.BaseDao;
-import com.dranawhite.forum.user.domain.BoardManagerPO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /**
  * @author dranawhite
--- test-forum/src/main/java/com/dranawhite/forum/user/dao/TopicMapper.java
+++ test-forum/src/main/java/com/dranawhite/forum/user/dao/TopicMapper.java
@@ -1,10 +1,7 @@
 package com.dranawhite.forum.user.dao;
 
 import com.dranawhite.forum.base.BaseDao;
-import com.dranawhite.forum.user.domain.TopicPO;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /**
  * @author dranawhite
