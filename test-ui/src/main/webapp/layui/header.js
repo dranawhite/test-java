73a6fce0a031dff17110ae7a3a18a514337d25e7
dranawhite <1458755325@qq.com>
2018-01-24 23:54:43 +0800

    layui——对话框

---
+++ test-ui/src/main/webapp/layui/header.js
@@ -0,0 +1,6 @@
+document.writeln('<link rel = "stylesheet" href =' +
+    ' "http://cdn.dranawhite.com:8080/dranawhite-ui/resources/plugin/layui/css/layui.css" />');
+document.writeln('<script type="application/javascript"' +
+    ' src="http://cdn.dranawhite.com:8080/dranawhite-ui/resources/js/jquery-3.2.1.min.js"></script>');
+document.writeln('<script type="application/javascript"' +
+    ' src="http://cdn.dranawhite.com:8080/dranawhite-ui/resources/plugin/layui/layui.js"></script>');
